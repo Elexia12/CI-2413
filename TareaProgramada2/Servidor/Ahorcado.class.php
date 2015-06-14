c92952c13b5b52e256b989950ca040e1db536670
= <=>
2015-06-14 17:12:23 -0600

    ordenando repo

---
+++ TareaProgramada2/Servidor/Ahorcado.class.php
@@ -0,0 +1,70 @@
+<?php
+
+class Ahorcado {
+	private $intentos = 8; //Aun no se cuantos, cuando hagamos la interfaz definimos. Si intentos llega a 0 pierde, se disminuye en 1 cada vez que el jugador falla
+	private $tiempoInicio;
+	private $tiempoFin;
+	private $palabras = array(/*posibles palabras a adivinar*/);
+	private $palabraJuego = ""; //palabra a adivinar
+	private $palabraOculta = ""; //Letras sustituidas por _
+	
+	//Se selecciona una palabra aleatoria de las definidas en $palabras
+	public function __construct() {
+		$rand = rand(0, sizeof($this->palabras) - 1);	// Un numero de 0 a cantidad de palabras - 1
+		$this->palabraJuego = $this->palabras[$rand];
+		//Sustituimos las letras por _
+		for($i=0; i<$strlen(this->palabraJuego); i++){
+			$this->palabraOculta = $this->palabraOculta . "_";
+		}
+		$this->tiempoInicio = time();
+	}
+	
+	//Tiempo que tardo en adivinar la palabra
+	public getTiempo(){
+		return $this->tiempoInicio - $this->tiempoFin;
+	}
+	
+	public getPalabraOculta(){
+		return $this->palabraOculta;
+	}
+	
+	//Recibe la letra digigtada por el jugador
+	//Retorna el estado actual de palabra oculta (despues del intento de adivinar)
+	public juego($letra){
+		$letra = strtolower($letra);
+		if(strpos($letra) === false){			//Si la letra no esta, se resta la cantidad de intentos
+			$this->intentos = Sthis->intentos - 1;
+		}else{
+			for($i=0; i<strlen($this->palabraJuego); i++){
+				if($this->palabraJuego[$i] == $letra){
+					$this->palabraOculta[$i] = $letra;			//Se cambia el _ por la letra encontrada
+				}
+			}
+		}
+		return $this->palabraOculta
+	}
+	
+	//Verifica el estado del juego
+	public verificarJuego(){
+		$res = "continuar";
+		if($this->palabraOculta == $this->palabraJuego){
+			$res = "gano";
+		}else{
+			if($this->intentos == 0){
+				$res = "perdio";
+			}
+		}
+		return $res;
+	}
+	
+	//Guarda el puntaje en la base de datos
+	public guardarPuntaje(){
+		
+	}
+	
+	//Retorna los diez mejores jugadores
+	public getMejores(){
+	
+	}
+	
+}
