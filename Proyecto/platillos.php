7aaa8b5c3e377c7b7f955460e320665f71e5df79
xiantra <sabusamomoshi@hotmail.com>
2015-07-17 02:33:17 -0600

    mañana sigo

--- Proyecto/platillos.php
+++ Proyecto/platillos.php
@@ -4,12 +4,13 @@
 	require_once('fatfree/lib/db/sql.php');
 	require_once('fatfree/lib/db/sql/mapper.php');
 	require_once('fatfree/lib/db/sql/session.php');
+	$db=new \DB\SQL('mysql:host=localhost;port=3306;dbname=ci2413_eb05206','eb05206','eb05206');
 	if(isset($_SESSION['permiso'])){
 		if($_SESSION['permiso'] == 2){
 			require("menu.php");
-			$db=new \DB\SQL('mysql:host=localhost;port=3306;dbname=ci2413_eb05206','eb05206','eb05206');
 			$platillos=$db->exec('SELECT * FROM platillos');
 			$categorias=$db->exec('SELECT * FROM categorias');
+			print_r($categorias);
 ?>
 <html>
   <head>
@@ -27,14 +28,20 @@
         <form id="formulario" method="POST" action="procesarlogin.php">
 			<input type="submit" value="Cerrar Sesi&oacute;n">
 		</form>
+		<br>
+		<p>Agregar Platillos</p>
         <form id="agregar" method="POST" action="acciones.php">
 			<p>Nombre:<input type="text" name="agregarnombre"></p>
 			<p>Precio:<input type="number" name="agregarprecio"></p>
 			<select name="agregarcategoria">
-				
+				<?php foreach($categorias as $categoria){ ?>
+					<option value='<?php echo $categoria['categorias_id']; ?>'><?php echo $categoria['nombre']; ?></option>
+				<?php } ?>
 			</select>
 			<input type="submit" value="Agregar">
 		</form>
+		<br>
+		<p>Editar Platillos</p>
 		<form id="editar" method="POST" action="acciones.php">
 			<select name="editarplato">
 				
@@ -42,10 +49,14 @@
 			<p>Nombre:<input type="text" name="editarnombre"></p>
 			<p>Precio:<input type="editarprecio"></p>
 			<select name="editarcategoria">
-				
+				<?php foreach($categorias as $categoria){ ?>
+					<option value='<?php echo $categoria['categorias_id']; ?>'><?php echo $categoria['nombre']; ?></option>
+				<?php } ?>
 			</select>
 			<input type="submit" value="Editar">
 		</form>
+		<br>
+		<p>Borrar Platillos</p>
 		<form id="borrar" method="POST" action="acciones.php">
 			<select name="borrarplato">
 				
